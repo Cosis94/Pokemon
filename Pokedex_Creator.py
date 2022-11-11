7b7c2e7866fe96c14c545bdba30fc562f4d6ec5a
Fatih Ekinci <Fatih.ekinci94@gmail.com>
2022-11-11 18:14:15 +0100

    test_2

--- Pokedex_Creator.py
+++ Pokedex_Creator.py
@@ -1,11 +1,20 @@
 import pandas as pd
+import re
+
 Full_List = []
 with open ("Pokedex.txt", "r", encoding="utf-8") as f:
     lines = f.readlines()[24:]
     for elements in lines:
         Full_List.append(elements.split())
 
+
+List_22 = []
+List_23 = []
+List_24 = []
+List_25 = []
+List_26 = []
 selected_List = []
+
 for x in Full_List:
     if "Fußnoten" in x:
         break
@@ -20,26 +29,54 @@ selected_List[0].pop(1)
 #print(selected_List[0])
 df = pd.DataFrame(data=selected_List)
 df = df[[1,8,10,12,14,22]]
+df = df.reset_index()
 #1 Nummer
 
-#print(df)
 #df.to_excel("Pokedex.xlsx")
-count = 0
+
+
+#Listen Creator with all not OK List Values
 new_List = []
+count = 0
 for x in selected_List:
-    if len(x) != 23:
-       # print(x)
-       # print(len(x))
-        count +=1
-       # print(count)
-        new_List.append(x)
+    if len(x) == 22:
+        List_22.append(x)
+    if len(x) == 24:
+        List_24.append(x)
+    if len(x) == 25:
+        List_25.append(x)
+    if len(x) == 26:
+        List_26.append(x)
 
-count = 0 
-for x in new_List:
-    print(x)
 
-    count +=1
-    print(len(x))
-    print(count)
-    print("\n")
-print(len(new_List))
+
+#dataframe_22 
+df_22 = pd.DataFrame(List_22)
+
+#dataframe_24 
+df_24 = pd.DataFrame(List_24)
+#dataframe_25
+df_25 = pd.DataFrame(List_25)
+#dataframe_26 
+df_26 = pd.DataFrame(List_26)
+
+#print(df)
+
+Liste_default = [re.sub(r'[0-9]*','', str(x)) for x in df[1]]
+
+#print(Liste_default)
+for x in range(0,len(Liste_default)):
+    if Liste_default[x]=="":
+        Liste_default[x] = 0
+    else:
+        Liste_default[x] = 1
+#print(Liste_default)
+
+df["Bool"] = Liste_default
+
+df_final = df[df["Bool"]==0]
+df_rest = df[df["Bool"]==1]
+
+
+
+print(len(df_rest))
