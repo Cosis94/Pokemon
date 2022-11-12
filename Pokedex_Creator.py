6ef66a89bfad263a1aecb865a33aa7dd06f28d44
Fatih Ekinci <Fatih.ekinci94@gmail.com>
2022-11-12 13:52:23 +0100

    Unterteilung in 3 DF
    Als nächstes sollten die DF zusammengeführt werden. DF sollte in for Schleifen bearbeitet werden

--- Pokedex_Creator.py
+++ Pokedex_Creator.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import re
+import numpy as np
 
 Full_List = []
 with open ("Pokedex.txt", "r", encoding="utf-8") as f:
@@ -52,15 +53,156 @@ for x in selected_List:
 
 #dataframe_22 
 df_22 = pd.DataFrame(List_22)
+df_22 = df_22.replace("||", "")
+df_22 = df_22.replace("|", "")
+df_22 = df_22.replace('style="height:',"")
+df_22 = df_22.replace('28px;"',"")
+df_22 = df_22.replace('<span',"")
+df_22 = df_22.replace([".+span>"],"",regex=True)
+df_22 = df_22.replace(["Tran.+"],"",regex=True)
+df_22 = df_22.replace(['.+Datei.+'],"",regex=True)
+df_22 = df_22.replace(['.+icon.+'],"",regex=True)
+df_22 = df_22.replace(['.+link.+'],"",regex=True)
+df_22 = df_22.replace(['.+früher.+'],"",regex=True)
+df_22 = df_22.replace(['Mr.'],"",regex=True)
+df_22 = df_22.replace(['Jr.'],"",regex=True)
+df_22 = df_22.replace(['M.$'],"",regex=True)
+df_22 = df_22.replace('/',"")
+df_22 = df_22[df_22!=""]
+df_22 = df_22.dropna(axis=1, how='all')
+df_22 = df_22.replace("[NaN]","",regex=True)
+print(df_22) 
+
 
 #dataframe_24 
 df_24 = pd.DataFrame(List_24)
+df_24 = df_24.replace("||", "")
+df_24 = df_24.replace("|", "")
+df_24 = df_24.replace('style="height:',"")
+df_24 = df_24.replace('28px;"',"")
+df_24 = df_24.replace('<span',"")
+df_24 = df_24.replace([".+span>"],"",regex=True)
+df_24 = df_24.replace(["Tran.+"],"",regex=True)
+df_24 = df_24.replace(['.+Datei.+'],"",regex=True)
+df_24 = df_24.replace(['.+icon.+'],"",regex=True)
+df_24 = df_24.replace(['.+link.+'],"",regex=True)
+df_24 = df_24.replace(['.+früher.+'],"",regex=True)
+df_24 = df_24.replace(['Mr.'],"",regex=True)
+df_24 = df_24.replace(['Jr.'],"",regex=True)
+df_24 = df_24.replace(['M.$'],"",regex=True)
+df_24 = df_24.replace('/',"")
+df_24 = df_24[df_24!=""]
+df_24 = df_24.dropna(axis=1, how='all')
+df_24 = df_24.replace("[NaN]","",regex=True)
+print(df_24) 
+
 #dataframe_25
 df_25 = pd.DataFrame(List_25)
+df_25 = df_25.replace("||", "")
+df_25 = df_25.replace("|", "")
+df_25 = df_25.replace('style="height:',"")
+df_25 = df_25.replace('28px;"',"")
+df_25 = df_25.replace('<span',"")
+df_25 = df_25.replace([".+span>"],"",regex=True)
+df_25 = df_25.replace(["Tran.+"],"",regex=True)
+df_25 = df_25.replace(['.+Datei.+'],"",regex=True)
+df_25 = df_25.replace(['.+icon.+'],"",regex=True)
+df_25 = df_25.replace(['.+link.+'],"",regex=True)
+df_25 = df_25.replace(['.+früher.+'],"",regex=True)
+df_25 = df_25.replace(['Mr.$'],"",regex=True)
+df_25 = df_25.replace(['Jr.$'],"",regex=True)
+df_25 = df_25.replace(['M.$'],"",regex=True)
+df_25 = df_25.replace('/',"")
+df_25 = df_25[df_25!=""]
+df_25 = df_25.dropna(axis=1, how='all')
+df_25 = df_25.replace("[NaN]","",regex=True)
+print(df_25) 
+
+
 #dataframe_26 
 df_26 = pd.DataFrame(List_26)
+df_26 = df_26.replace("||", "")
+df_26 = df_26.replace("|", "")
+df_26 = df_26.replace('style="height:',"")
+df_26 = df_26.replace('28px;"',"")
+df_26 = df_26.replace('<span',"")
+df_26 = df_26.replace([".+span>"],"",regex=True)
+df_26 = df_26.replace(["Tran.+"],"",regex=True)
+df_26 = df_26.replace(['.+Datei.+'],"",regex=True)
+df_26 = df_26.replace(['.+icon.+'],"",regex=True)
+df_26 = df_26.replace(['.+link.+'],"",regex=True)
+df_26 = df_26.replace(['.+früher.+'],"",regex=True)
+df_26 = df_26.replace(['Mr.'],"",regex=True)
+df_26 = df_26.replace(['Jr.'],"",regex=True)
+df_26 = df_26.replace(['M.$'],"",regex=True)
+df_26 = df_26.replace('/',"")
+df_26 = df_26[df_26!=""]
+df_26 = df_26.dropna(axis=1, how='all')
+df_26 = df_26.replace("[NaN]","",regex=True)
+print(df_26) 
 
-#print(df)
+
+
+
+#Unterteilung in 3 Fälle
+
+df_22_1 = df_22[df_22[1]=="???"]
+df_22_1 = df_22_1.dropna(axis=1, how='all')
+df_22_2 = df_22[df_22[1].isna()]
+df_22_2 = df_22_2.dropna(axis=1, how='all')
+df_22_3 = df_22[df_22[1]!="???"]
+df_22_3 = df_22_3[df_22_3[1].notna()]
+df_22_3 = df_22_3.dropna(axis=1, how='all')
+print(df_22_2)
+print(df_22_1)
+print(df_22_3)
+
+
+df_24_1 = df_24[df_24[1]=="???"]
+df_24_1 = df_24_1.dropna(axis=1, how='all')
+df_24_2 = df_24[df_24[1].isna()]
+df_24_2 = df_24_2.dropna(axis=1, how='all')
+df_24_3 = df_24[df_24[1]!="???"]
+df_24_3 = df_24_3[df_24_3[1].notna()]
+df_24_3 = df_24_3.dropna(axis=1, how='all')
+print(df_24_2)
+print(df_24_1)
+print(df_24_3)
+
+
+df_25_1 = df_25[df_25[1]=="???"]
+df_25_1 = df_25_1.dropna(axis=1, how='all')
+df_25_2 = df_25[df_25[1].isna()]
+df_25_2 = df_25_2.dropna(axis=1, how='all')
+df_25_3 = df_25[df_25[1]!="???"]
+df_25_3 = df_25_3[df_25_3[1].notna()]
+df_25_3 = df_25_3.dropna(axis=1, how='all')
+print(df_25_2)
+print(df_25_1)
+print(df_25_3)
+
+
+df_26_1 = df_26[df_26[1]=="???"]
+df_26_1 = df_26_1.dropna(axis=1, how='all')
+df_26_2 = df_26[df_26[1].isna()]
+df_26_2 = df_26_2.dropna(axis=1, how='all')
+df_26_3 = df_26[df_26[1]!="???"]
+df_26_3 = df_26_3[df_26_3[1].notna()]
+df_26_3 = df_26_3.dropna(axis=1, how='all')
+print(df_26_2)
+print(df_26_1)
+print(df_26_3)
+
+
+
+
+
+
+
+
+
+
+
 
 Liste_default = [re.sub(r'[0-9]*','', str(x)) for x in df[1]]
 
@@ -75,8 +217,23 @@ for x in range(0,len(Liste_default)):
 df["Bool"] = Liste_default
 
 df_final = df[df["Bool"]==0]
-df_rest = df[df["Bool"]==1]
+#df_rest = df[df["Bool"]==1]
+
+#print(df_final)
 
 
 
-print(len(df_rest))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
